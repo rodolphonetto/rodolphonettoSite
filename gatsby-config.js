31608addbadaa7d0ca8b25791edc1de9ce8a6e19
Rodolpho Netto <rodolpho.netto@gmail.com>
2020-01-23 11:54:14 -0300

    Atualizada conexão para api de produção e desenvolvimento

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,9 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL
+          ? "https://rodolphonetto-api.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "projeto",
