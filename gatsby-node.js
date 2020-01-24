a2499fddec455865aa6399a0ac4f04a8852ae0ec
Rodolpho Netto <rodolpho.netto@gmail.com>
2020-01-23 21:15:10 -0300

    Crição das paginas com slugs dinamicos [wip]

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,56 @@
+const path = require("path")
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
+
+  const createSlug = name => {
+    if (name) {
+      const slug = name.replace(" ", "-").toLowerCase()
+      return slug
+    }
+    return ""
+  }
+
   // Creates new query'able field with name of 'slug'
   createNodeField({
     node,
     name: "slug",
-    value: `/${node.name + "teste"}`,
+    value: `/${createSlug(node.name)}`,
+  })
+}
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
+      query Projetos {
+        allStrapiProjeto {
+          nodes {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  // Create pages for each node.
+  const ProjectPage = path.resolve(`./src/pages/project.js`)
+  result.data.allStrapiProjeto.nodes.forEach(node => {
+    createPage({
+      path: node.fields.slug,
+      component: ProjectPage,
+      context: {
+        slug: node.fields.slug,
+      },
+    })
   })
 }
---
+++ src/pages/project.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout/Layout"
+import Header from "../components/Header/Header"
+import Title from "../components/Title/Title"
+
+const ProjectPage = ({ data, pageContext }) => {
+  console.log(pageContext.slug) //retorna o slug correto!!!
+  return (
+    <Layout>
+      <Header />
+      <Title>{data.strapiProjeto.name}</Title>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query Post($slug: String!) {
+    strapiProjeto(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
+      name
+    }
+  }
+`
+export default ProjectPage
